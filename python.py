ba04f1c475e3159fb909a1566517641b3dab4873
Talha_Razzaq <105111642+talha-razzaq@users.noreply.github.com>
2023-09-21 09:12:21 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+# 1st Madlibs Program
+
+name = input("Enter name : ")
+country = input("Enter country : ")
+subject = input("Enter subject: ")
+adj = input("Enter adj : ")
+verb1 = input("Enter verb1 : ")
+verb2 = input("Enter verb2 : ")
+
+intro = f"My name is {name}!. I am from {country}!. Well I'm {subject} student. Programming is so {adj}!. I love this! because it makes me so excited all the time, I think it's like {verb1}!. So, It's very {verb2}"
+
+print(intro)
