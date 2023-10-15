a119fb757ebf343ec63d56c41602e1efaba26eff
Talha_Razzaq <105111642+talha-razzaq@users.noreply.github.com>
2023-10-15 09:03:12 +0500

    Add files via upload

---
+++ character_input.py
@@ -0,0 +1,12 @@
+#### Character input
+
+#take two input
+name = input("Enter the name : ")
+age = int(input("Enter the age : "))
+
+#varible
+
+address = 2023-age+100
+
+print(name," is your name and your age are ", age, "and you will be handred in ", address,".")
+
