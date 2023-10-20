a2057bbbdbd712cf407fb95641d2c1c5de1452fa
Talha_Razzaq <105111642+talha-razzaq@users.noreply.github.com>
2023-10-20 08:11:31 +0500

    Add files via upload

---
+++ list_comprehension.py
@@ -0,0 +1,11 @@
+# List comprehension
+
+numbers = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+even_list = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_list.append(num)
+        even_list.sort()
+
+print(even_list)
